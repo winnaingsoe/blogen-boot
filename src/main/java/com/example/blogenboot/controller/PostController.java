d3d05e5add987eef2e4cc547a9188c327808c0f6
winnaingsoe <winnaingsoe@outlook.com>
2022-12-20 11:54:15 +0630

    Post Entry

--- src/main/java/com/example/blogenboot/controller/PostController.java
+++ src/main/java/com/example/blogenboot/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.example.blogenboot.controller;
 
 import com.example.blogenboot.ds.Category;
+import com.example.blogenboot.ds.Post;
 import com.example.blogenboot.ds.User;
 import com.example.blogenboot.service.BlogenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +23,7 @@ public class PostController {
     public String index(Model model) {
         model.addAttribute("category", new Category());
         model.addAttribute("user", new User());
+        model.addAttribute("post", new Post());
         return "dashboard";
     }
     @PostMapping("/save-category")
@@ -40,15 +43,40 @@ public class PostController {
         blogenService.saveUser(user);
         return "redirect:/list-users";
     }
+    @PostMapping("/save-post")
+    public String savePost(Post post, BindingResult result) {
+        if(result.hasErrors()) {
+            return "dashboard";
+        }
+        blogenService.savePost(post);
+        return "redirect:/list-posts";
+    }
 
     @GetMapping("/list-categories")
-    public String listAllCategories(Model model) {
-        model.addAttribute("categories", blogenService.findAllCategories());
+    public String listAllCategories(Model model, @ModelAttribute("categories") List<Category> categories) {
+        model.addAttribute("categories", categories);
         return "list-categories";
     }
     @GetMapping("/list-users")
-    public String listAllUsers(Model model) {
-        model.addAttribute("users", blogenService.findAllUsers());
+    public String listAllUsers(Model model, @ModelAttribute("users") List<User> users) {
+        model.addAttribute("users", users);
         return "list-users";
+    }
+    @GetMapping("/list-posts")
+    public String listAllPosts(Model model, @ModelAttribute("posts") List<Post> posts) {
+        model.addAttribute("posts", posts);
+        return "list-posts";
+    }
+    @ModelAttribute("categories")
+    public List<Category> listCategories() {
+        return blogenService.findAllCategories();
+    }
+    @ModelAttribute("users")
+    public List<User> listUsers() {
+        return blogenService.findAllUsers();
+    }
+    @ModelAttribute("posts")
+    public List<Post> listPosts() {
+        return blogenService.findAllPosts();
     }
 }
--- src/main/java/com/example/blogenboot/service/BlogenService.java
+++ src/main/java/com/example/blogenboot/service/BlogenService.java
@@ -4,9 +4,11 @@ import com.example.blogenboot.dao.CategoryDao;
 import com.example.blogenboot.dao.PostDao;
 import com.example.blogenboot.dao.UserDao;
 import com.example.blogenboot.ds.Category;
+import com.example.blogenboot.ds.Post;
 import com.example.blogenboot.ds.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,7 +33,20 @@ public class BlogenService {
         return userDao.findAll();
     }
 
+    public List<Post> findAllPosts() {
+        return postDao.findAll();
+    }
+
     public void saveUser(User user) {
         userDao.save(user);
     }
+
+    @Transactional
+    public void savePost(Post post) {
+        post.setCategory(categoryDao.findById(post.getCategory().getId()).get());
+        post.setUser(userDao.findById(post.getUser().getId()).get());
+        postDao.save(post);
+    }
+
+
 }
